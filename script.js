c662d20c5c249f48ff9c6cbe135bdb329c96f042
AehniCen <Anika.Schmidt1992@web.de>
2025-07-23 13:13:03 +0200

    add Pokemon Database, add Pokemon-Detais-Overlay, add Pokemon-Details Template func.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let allDetails = [];
+
 function toggleOverlay() {
     let overlayRef = document.getElementById('pokedex-overlay');
     let pokedexImg = document.getElementById('pokedex-closed-img');
@@ -11,13 +13,78 @@ function toggleOverlay() {
     }, 2000);
 }
 
-async function loadPokemon() {
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon/ivysaur');
-    let pokemon = await response.json();
-    let pokemonRef = document.getElementById('poke-div');
-    pokemonRef.innerHTML += pokeTemplate(pokemon);
+function togglePokemonOverlay(index) {
+    let pokeOverlayRef = document.getElementById('pokemon-details-div');
+    pokeOverlayRef.classList.toggle('d_none');
+    let pokemon;
 
+    if (index !== undefined) {
+        pokemon = allDetails[index];
+        applyTypeColor(pokemon);
+    }
+
+    if (pokemon) {
+        renderPokeDetails(pokemon, index);
+    }
 
     console.log(pokemon);
     
+}
+
+function applyTypeColor(pokemon) {
+    let pokeOverlayRef = document.getElementById('pokemon-details-div');
+    let mainType = pokemon.types[0].type.name;
+
+    pokeOverlayRef.classList.add(`type-${mainType}`);
+}
+
+function renderPokeDetails(pokemon, index) {
+    let pokeOverlayRef = document.getElementById('pokemon-details-div');
+
+    pokeOverlayRef.innerHTML = "";
+    pokeOverlayRef.innerHTML += pokeDetailsTemplate(pokemon, index);
+}
+
+function renderTypes() {
+    let typesRef = document.getElementById('types-div');
+
+    for (let type in typeIcons) {
+        let typeSrc = typeIcons[type];
+        typesRef.innerHTML += `<div id="types-category-div"><img class="types-categories" src="${typeSrc}" alt="type-icon"> <span class="type-text">${type}</span> </div>`;
+    }
+    
+}
+
+async function loadPokemon() {
+   
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+    let data = await response.json();
+
+    for (let pokemon of data.results) {
+        let detailsResponse = await fetch(pokemon.url);
+        let details = await detailsResponse.json();
+        allDetails.push(details);
+    }
+
+    allDetails.forEach((pokemon, index) => renderPokeMenu(pokemon, index)); 
+}
+
+function renderPokeMenu(pokemon, index) {
+    let pokeRef = document.getElementById('poke-div');  
+    pokeRef.innerHTML += pokeTemplate(pokemon, index);
+    renderTypeIcon(pokemon, index);
+}
+
+function renderTypeIcon(pokemon, index) {
+    let typeRef = document.getElementById(`poke-div-type-${index}`);
+    typeRef.innerHTML = '';
+
+    pokemon.types.forEach(t => {
+      const typeName = t.type.name;
+      const iconPath = typeIcons[typeName];
+
+      if (iconPath) {
+        typeRef.innerHTML += `<img id="poke-type-icon" src="${iconPath}" alt="type-icon">`;
+      }
+    });
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,40 @@
-pokeTemplate(pokemon){
+function pokeTemplate(pokemon, index){
     return `
-        <div class="poke-img"><img src:"${pokemon.sprites.other.offical-artwork.front_default}" alt:"pokemon-image">
-    `;
-};
+    <div class="template-div" onclick="togglePokemonOverlay(${index})">
+        <div class="poke-div-type" id="poke-div-type-${index}"></div>
+        <div class="poke-div-img"><img id="poke-img-image"src="${pokemon.sprites.other["official-artwork"].front_default}" alt="pokemon-image"></div>
+        <div class="poke-div-name"><p id="poke-name-p">${pokemon.species.name}</p></div>
+    </div>
+    `};
+
+function pokeDetailsTemplate(pokemon, index) {
+    return `
+    <div id="poke-details-div">
+        <button id="poke-details-button">x</button>
+        <div id="poke-details-img-div">
+            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="">
+        </div>
+        <div id="poke-details-name-div">
+            <p id="poke-details-name">${pokemon.species.name}</p>
+            <div id="poke-details-type">${pokemon.types
+                    .map(t => {
+                      const type = t.type.name;
+                      const icon = typeIcons[type];
+                      return `
+                            <div class="poke-details-types-div">
+                                <img id="poke-details-type-icons" src="${icon}" alt="${type}" class="type-icon">
+                                <span class="poke-details-type-name">${type}</span>
+                            </div>
+                            `;
+                    })
+                    .join('')}
+            </div>
+        </div>
+        <div id="poke-details-stats-div">
+            <p id="poke-details-abilities">${pokemon.abilities.map(t => t.ability.name).join(' / ')}</p>
+            <p id="poke-details-moves">${pokemon.moves.slice(0, 3).map(t => t.move.name).join(' / ')}</p>
+            <p id="poke-details-stats">${pokemon.stats.map(t => t.stat.name).join(' / ')}</p>
+        </div>
+    </div>
+    `
+}
